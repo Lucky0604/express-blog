26783a2edf0406c72aaa1255e246eddc87233fc8
Lucky <175917645@qq.com>
2016-04-11 00:13:26 +0800

    basic file structure

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+var routes = require('./routes/index');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: settings.cookieSecret,
+    key: settings.db, //cookie name
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},         // 30days
+    store: new MongoStore({
+        db: settings.db,
+        host: settings.host,
+        port: settings.port
+    })
+}))
+
+routes(app);
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// })
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         })
+//     })
+// }
+
+
+// production error handler
+// no stacktrace leaked to user
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: err
+//     })
+// })
+
+app.listen(app.get('port'), function() {
+    console.log('Express server running on port ' + app.get('port'));
+})
