4f5b7ac9e4f6cf71d2c42351e02544801b7ba110
Lucky <175917645@qq.com>
2016-04-11 23:14:38 +0800

    add basic view pages

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
+var settings = require('./settings');
 
 var routes = require('./routes/index');
 var app = express();
@@ -23,9 +24,12 @@ app.use(session({
     key: settings.db, //cookie name
     cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},         // 30days
     store: new MongoStore({
+        /*
         db: settings.db,
         host: settings.host,
         port: settings.port
+        */
+        url: 'mongodb://localhost/blog'
     })
 }))
 
